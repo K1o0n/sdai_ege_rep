6320a9a2028b5f711e492026ea3300cd8f8c43d0
Nikolas-Kravtsov <63307366+Nikolas-Kravtsov@users.noreply.github.com>
2025-01-30 15:15:49 +0300

    Update graph_functions.py

--- graph_functions.py
+++ graph_functions.py
@@ -30,12 +30,16 @@ def convert_attempts_by_months(user_id):
 def convert_attempts_by_type(user_id):
     all_attempts = get_attempts_by_task_type(user_id)
     cur_index = 0
-    result = {}
+    result1 = []
+    result2 = []
+    resukt3 = []
     correct = 0
     wrong = 0
     while cur_index < len(all_attempts):
         if cur_index == 0 or all_attempts[cur_index][2] == all_attempts[cur_index - 1][2]:
-            result[all_attempts[cur_index][3]] = (correct, wrong)
+            result1.append(all_attempts[cur_index][3])
+            result2.append(correct)
+            result3.appen(wrong)
             correct = 0
             wrong = 0
         else:
@@ -43,4 +47,4 @@ def convert_attempts_by_type(user_id):
                 correct += 1
             else:
                 wrong += 1
-    return result
+    return result1, result2, result3
